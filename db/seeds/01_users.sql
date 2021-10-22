32c1a3d14290cc59190b00168d148bc89e0452db
jenobpj <jenobgit@gmail.com>
2021-10-22 13:30:16 -0400

    add fake data for testing

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,10 @@
--- -- Users table seeds here (Example)
--- INSERT INTO users (name) VALUES ('Alice');
--- INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password, phone)
+VALUES
+  ('Andy Apple', 'apple@gmail.com', 'password', '519-303-3030'),
+  ('Julie Orange', 'orange@gmail.com', 'password', '647-898-9898'),
+  ('Manny Mango', 'mango@gmail.com', 'password', '123-456-7890'),
+  ('Peter Pear', 'pear@gmail.com', 'password', '345-543-3456'),
+  ('Gary Grape', 'grape@gmail.com', 'password', '234-432-2341'),
+  ('Penny Pineapple', 'pineApple@gmail.com', 'password', '567-897-6787'),
+  ('Adam Administrator', 'aadmin@gmail.com', 'password', '519-999-9595');
+
---
+++ db/seeds/02_cars.sql
@@ -0,0 +1,85 @@
+INSERT INTO cars(seller_id,make,model,model_colour,year,doors,descriptions,thumbnail_url,cover_url,car_price)
+VALUES(1,'HONDA','ACCORD','RED',2018,4,'NO ACCIDENTS / 1 OWNER / CLEAN CARFAX / and more!
+
+All Clutch cars are SAFETY CERTIFIED, come with a 10-day / 750km MONEY-BACK GUARANTEE and FREE TO-YOUR-DOOR DELIVERY.
+
+Purchases can be completed 100% online and have no hidden fees. Skip the dealership and buy your next car online with Clutch!
+
+Click Visit Dealer Website or go to Clutch.ca to view 360-degree photos, 210-point inspection reports, and to begin the online checkout process.
+
+Standout Features for this 2018 Honda Accord include:
+*Apple CarPlay
+*Rearview Camera
+*Android Auto
+*Keyless Entry
+*Heated Front Seats
+*Bluetooth','https://tdrpmimages.azureedge.net/photos/import/202110/1917/0620/9c81d127-9030-4553-9ebb-fa9aee5344a1_overlay.jpg-180x135','https://tdrpmimages.azureedge.net/photos/import/202110/1917/0620/9c81d127-9030-4553-9ebb-fa9aee5344a1_overlay.jpg-1024x786',25990),
+(2,'HONDA','ACCORD COUPE','BLACK',2017,2,'We would like to inform our customers that we remain essential and are open by appointment and online basis. Visit our website to apply for financing and to estimate your monthly payments. Take a look at our inventory of quality used vehicles. For the protection of our customers and staff our dealership kindly asks all guests to have a face mask. All of our vehicles are certified professionally detailed and we have access to the local service Ontario which remains open for dealer transactions.
+
+No Accidents
+
+Touring Model
+
+6 Speed Manual
+
+Leather
+
+Sunroof
+
+Factory Skirt Package
+
+Wireless Phone Charger
+
+Honda Link Bluetooth & Much More
+
+CARFAX LINK:
+
+**********
+
+
+
+***Price Includes (Safety) Certification, Emissions Test. No other Fees. Powertrain Warranty Available.
+
+*** FINANCING AVAILABLE
+
+Call Now
+
+**********
+
+4732 SHEPPARD AVE EAST SCARBOROUGH ON M1S 3V6','https://tdrpmimages.azureedge.net/photos/202110/1404/2543/3612aeb3-60df-4842-adc2-ae5af1e2fe66.jpg-180x135','https://tdrpmimages.azureedge.net/photos/202110/1404/2543/3612aeb3-60df-4842-adc2-ae5af1e2fe66.jpg-1024x786',20999),
+(3,'HYUNDAI','SONATA LUXURY','BLACK',2021,4,'8 x 7.5J Alloy Wheels, ABS brakes, Air Conditioning, AM/FM radio: SiriusXM, AppLink/Apple CarPlay and Android Auto, Automatic temperature control, Brake assist, Dual front impact airbags, Dual front side impact airbags, Exterior Parking Camera Rear, Four wheel independent suspension, Front anti-roll bar, Fully automatic headlights, Heated door mirrors, Heated Front Bucket Sport Seats, Heated steering wheel, Knee airbag, Leather Seating Surfaces w/Red Stitching, Leather Shift Knob, Leather steering wheel, Navigation System, Occupant sensing airbag, Overhead airbag, Overhead console, Power door mirrors, Power moonroof, Power steering, Power windows, Radio: Infinity AM/FM/XM/MP3 w/Navigation System, Rear Parking Sensors, Rear window defroster, Remote keyless entry, Security system, Split folding rear seat, Steering wheel mounted audio controls, Turn signal indicator mirrors.
+
+
+Shop 100% online, from the comfort of your own home. Our team members are here to help you via phone, email, text or FaceTime. You can start the buying process online with many of our tools like choosing your vehicle, configuring a payment, getting pre-approval for financing, appraising your trade and even completing the entire process online. We will then schedule a time and place to bring your vehicle to you for delivery.','https://tdrpmimages.azureedge.net/photos/202110/0101/3223/07ca9557-7f81-4132-93b0-964e92c3b8ea.jpg-180x135','https://tdrpmimages.azureedge.net/photos/202110/0101/3223/07ca9557-7f81-4132-93b0-964e92c3b8ea.jpg-1024x786',20688),
+(4,'HYUNDAI','TUSCON ESSENTIAL','SILVER',2022,4,'nly $193 Weekly, with $0 down payment!
+
+Lowest Price Guaranteed on all new Hyundai models!
+Find a lower quote? Bring it to Lauria and well beat it!
+
+The 2021 Palisade, as stunning as it is accommodating, is more than just a pretty face. The spacious and airy Palisade is designed to indulge, with plush seating for 8, impressive premium tech, and safety advances for unparalleled peace of mind. Get the family ready. Palisade is sure to impress!
+
+All-in pricing prices include EVERYTHING but taxes and licensing.
+
+Financing example: Vehicle price is $10,000 financed over 96mo. term @ 4.9% APR. $0 Down payment. Cost of borrowing is $2,107.87. OAC. Plus taxes. Please call for more details.
+
+Port Hopes Lauria Hyundai is a family-run dealership and has been servicing the regions of Clarington, Oshawa, Durham, Peterborough, Lindsay, Belleville, Cobourg, Whitby, Ajax, Bowmanville, and Northumberland since 1984. As multiple Presidents Award of Merit winners, we were honoured to be named Canadas number 1 overall Hyundai dealership in 2012 and have twice been ranked number 1 overall in total customer satisfaction since 2010! Satisfied customers are our key to success, give us a chance to prove it to you, we wont let you down! Think Smart, Think Lauria Hyundai.
+
+Lowest Price Guarantee applies to a written or emailed quote on an in-stock, current model year, equivalent Hyundai vehicle. Please call, stop by or contact us online for more details.','https://tdrpmimages.azureedge.net/photos/import/202110/1911/2819/5d07edeb-ea27-4743-aa3b-0296d9d15b44.jpg-180x135','https://tdrpmimages.azureedge.net/photos/import/202110/1911/2819/5d07edeb-ea27-4743-aa3b-0296d9d15b44.jpg-1024x786',29924),
+(5,'FORD','F-150','WHITE',2018,4,'THIS 2021 FORD F-150 SUPERCREW XLT INCLUDES A 3.5L V6 ECOBOOST ENGINE, BLACK SPORT INTERIOR, FORDPASS CONNECT AND SYNC 4, LANE KEEPING SYSTEM, PRE-COLLISION ASSIST, REAR VIEW CAMERA, BOXLINK CARGO SYSTEM, TRAILER TOW PACKAGE WITH INTEGRATED TRAILER BRAKE CONTROL, TAILGATE STEP, XLT SPORT PACKAGE, 20" WHEELS AND MUCH MORE!!
+
+Grimsby Ford is always sure to advertise the LOWEST possible price - ALL available rebates and incentives have been applied. Lower finance rates may also be available. Please feel free to call or request a VIP appointment with one of our managers to review the right option for you.
+
+Advertised Sale Price is valid for eligible Finance and Lease customers see dealer for details.
+
+Call today to book your ultimate test drive experience! !
+
+Selling price does not include HST & Licensing.
+
+Sold Safety checked under Government Guidelines by a Certified Ford Trained Technician.
+
+Ford Certified Series Extended Warranty available at additional cost.
+
+
+
+Get a Free CARFAX CANADA Report with every pre-owned vehicle purchased.','https://tdrpmimages.azureedge.net/photos/202110-1514-1630/6f784193-b009-477d-a0b7-2838f54d006b_overlay.jpg-180x135','https://tdrpmimages.azureedge.net/photos/202110-1514-1630/6f784193-b009-477d-a0b7-2838f54d006b_overlay.jpg-1024x786',61149),
+(6,'FORD','MUSTANG','GREEN',2021,4,'2020 FORD SHLEBY GT500 | 760 HP | 5.2L V8 | 7 SPEED DUAL CLUTCH | GOLDEN TICKET | CARBON FIBER TRACK PACKAGE | CARBON FIBER WHEELS | CARBON FIBER FRONT LIP AND WING | DUAL COLOUR RECARO SEATS | NAVIGATION | BACKUP CAMERA | TECHNOLOGY PACKAGE | LEATHER | ACTIVE EXHAUSTS | BANG & OLUFSEN PREMIUM SOUND SYSTEM | EXPOSED CARBON FIBRE INTERIOR | REAR SEAT DELETE | REMOTE KEYLESS ENTRY | BLINDSPOT MONITORING | BLUETOOTH | APPLE CARPLAY | SIRIUS XM | CANADIAN VEHICLE | CLEAN CARFAX','https://tdrpmimages.azureedge.net/photos/import/202105/2520/2200/143f3299-3fcd-43d5-bfcc-abc39c8232ed.jpg-180x135','https://tdrpmimages.azureedge.net/photos/import/202105/2520/2200/143f3299-3fcd-43d5-bfcc-abc39c8232ed.jpg-1024x786',166800)
