2ea66c225055fa33c55852970a61245b2c8da9aa
jenobpj <jenobgit@gmail.com>
2021-10-24 14:14:48 -0400

    modify routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const path=require('path')
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -30,18 +31,19 @@ app.use(
     isSass: false, // false => scss, true => sass
   })
 );
-
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,'public')))
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+// const usersRoutes = require("./routes/users");
+// const widgetsRoutes = require("./routes/widgets");
+const carshow=require("./routes/carshowing")
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+// app.use("/api/users", usersRoutes(db));
+// app.use("/api/widgets", widgetsRoutes(db));
+app.use("/newcars",carshow(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -51,6 +53,8 @@ app.use("/api/widgets", widgetsRoutes(db));
 app.get("/", (req, res) => {
   res.render("landing");
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
